a1c509dedaa4dc04b22f19dca50c497b4cc66c82
amandaptuar <amandaptuar@gmail.com>
2026-04-03 13:17:49 +0530

    fixed the loading error

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import Preloader from './Preloader';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -21,14 +20,21 @@ const loadScript = (src) =>
 const Layout = () => {
   useEffect(() => {
     const initTemplateScripts = async () => {
+      // 1. Load jQuery first
       await loadScript('/assets/js/plugins/jquery-3.7.1.min.js');
-      await loadScript('/assets/js/plugins/isotope.pkg.min.js');
-      await loadScript('/assets/js/plugins/jquery.slick.min.js');
-      await loadScript('/assets/js/plugins/odometer.js');
-      await loadScript('/assets/js/plugins/ripples.min.js');
-      await loadScript('/assets/js/plugins/wow.min.js');
-      await loadScript('/assets/js/plugins/swiper.min.js');
-      await loadScript('/assets/js/plugins/YTPlayer.min.js');
+      
+      // 2. Load all jQuery plugins in parallel
+      await Promise.all([
+        loadScript('/assets/js/plugins/isotope.pkg.min.js'),
+        loadScript('/assets/js/plugins/jquery.slick.min.js'),
+        loadScript('/assets/js/plugins/odometer.js'),
+        loadScript('/assets/js/plugins/ripples.min.js'),
+        loadScript('/assets/js/plugins/wow.min.js'),
+        loadScript('/assets/js/plugins/swiper.min.js'),
+        loadScript('/assets/js/plugins/YTPlayer.min.js')
+      ]);
+
+      // 3. Load main.js after plugins are ready
       await loadScript('/assets/js/main.js');
     };
 
@@ -37,7 +43,6 @@ const Layout = () => {
 
   return (
     <>
-      <Preloader />
       <Header />
       <Outlet />
       <Footer />
